fc9dcf601af112de0da225a72090361bcac146c9
Swastika Gupta <swastikagupta.2305@gmail.com>
2024-12-04 16:12:56 +0530

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer(){
+    return(
+        
+        <footer className="footer-title footer-font">
+           <p>© 2045 All Rights Reserved. By HTML Design</p>
+            
+         </footer>
+         
+
+    );
+};
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header(){
+    return(
+        <div className="header-title header-font">
+            <h1>My react App</h1>
+        </div>
+
+    );
+};
